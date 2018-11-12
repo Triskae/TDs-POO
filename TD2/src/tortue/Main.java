d404638f4fcee8474ad76b813a77d141b0a31918
Filipe Doutel Silva <filipe.doutelsilva@hotmail.fr>
2018-11-12 16:12:22 +0100

    Ajout de la classe Main du TD2

--- TD2/src/tortue/Main.java
+++ TD2/src/tortue/Main.java
@@ -1,4 +1,17 @@
 package tortue;
 
 public class Main {
+    public static void main(String[] args) {
+        Tortue t = new Tortue();
+
+        System.out.println(t);
+        t.avancer(50);
+        t.goRight(90);
+        t.avancer(50);
+        t.goRight(90);
+        t.reculer(50);
+        t.goRight(90);
+        t.reculer(50);
+        System.out.println(t);
+    }
 }
